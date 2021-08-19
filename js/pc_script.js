3315c4e756fc7b22f4009f3e6c85ee64aea44945
web-dev-faruk <omurfaruk4250@gmail.com>
2021-08-18 19:02:48 -0700

    First Commit

---
+++ js/pc_script.js
@@ -0,0 +1,11 @@
+// function myFunction(x) {
+//     if (x.matches) { // If media query matches
+//       document.body.style.backgroundColor = "yellow";
+//     } else {
+//      document.body.style.backgroundColor = "pink";
+//     }
+//   }
+  
+//   var x = window.matchMedia("(max-width: 700px)")
+//   myFunction(x) // Call listener function at run time
+//   x.addListener(myFunction) // Attach listener function on state changes
